7b43f0a9e2ecfbe302f9cd2a2146aa0577cfadf8
peeyush2025 <sudhakarln82@gmail.com>
2025-09-27 19:19:27 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,12 @@
+# take user input
+a=input("enter a word: ")
+# program to check break keyword
+for i in a:# iterate for loop
+    if (i== 'a'):# condition 1
+        # display result
+        print("a is found")
+        break # break statement
+    else:
+    #display result
+        print("a not found")
+        
